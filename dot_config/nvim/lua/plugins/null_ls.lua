77350031bfc635296c7be187b93506ea4b92cc92
Max Enelund <max@ig4.se>
2023-04-24 21:38:15 +0200

    feat(nvim): i dont even know

--- dot_config/nvim/lua/plugins/null_ls.lua
+++ dot_config/nvim/lua/plugins/null_ls.lua
@@ -1,12 +1,12 @@
 return {
     'jose-elias-alvarez/null-ls.nvim',
     dependencies = { 'nvim-lua/plenary.nvim', 'lsp-zero.nvim' },
-    lazy = true,
     config = function()
         local null_ls = require('null-ls')
 
         null_ls.setup({
             sources = {
+                null_ls.builtins.formatting.prettier,
                 null_ls.builtins.formatting.stylua,
             },
         })
