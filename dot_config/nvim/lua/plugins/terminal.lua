6a588d5889da56923e184ab7121efb52d83c53fb
Max Enelund <max@ig4.se>
2023-04-25 23:07:21 +0200

    feat(something): thinkge

---
+++ dot_config/nvim/lua/plugins/terminal.lua
@@ -0,0 +1,19 @@
+return {
+    'akinsho/toggleterm.nvim',
+    config = function()
+        require('toggleterm').setup({
+            size = 20,
+            hide_numbers = true,
+            shading_factor = 2,
+        })
+
+        local opts = { silent = true }
+        vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+        vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+        vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+        vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+        vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+        vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+        vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+    end,
+}
